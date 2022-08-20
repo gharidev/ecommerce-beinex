96e71779933090a1202d7512d908f86deb996ed7
G Haridev <haridevgireesh@gmail.com>
2022-08-20 14:43:57 +0530

    Added models & migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+# CUSTOM USER MODEL
+class User(AbstractUser):
+    """Custom user model."""
+    class Types(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        CUSTOMER = 'CUSTOMER', 'Customer'
+
+    type = models.CharField('Type', max_length=50, choices=Types.choices, default=Types.CUSTOMER)
+
+# USER MANAGERS
+class AdminManager(UserManager):
+    """User manager for `Admin`."""
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(type=User.Types.ADMIN)
+
+class CustomerManager(UserManager):
+    """User manager for `Customer`."""
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(type=User.Types.CUSTOMER)
+
+# PROXY USER MODELS
+class Admin(User):
+    """Proxy user model for administrators."""
+    class Meta:
+        proxy = True
+
+    objects = AdminManager
+
+class Customer(User):
+    """Proxy user model for customers"""
+    class Meta:
+        proxy = True
+
+    objects = CustomerManager
+
+# PRODUCT & CATEGORY MODEL
+class Product(models.Model):
+    """Product model."""
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField('Category')
+    subcategories = models.ManyToManyField('SubCategory')
+
+class Category(models.Model):
+    """Category model."""
+    name = models.CharField(max_length=255)
+
+class SubCategory(models.Model):
+    """SubCategory model."""
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
--- ecommerce_beinex/settings.py
+++ ecommerce_beinex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g@l)l9+4i@y!l9u#xax!3(%nggd)@xb@o0+8jx#3c3=icf#$$='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.User'
